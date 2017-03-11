55a3102cc1e807e64cd15a258a8e04d8d5dd9a27
numb86 <86numb@gmail.com>
2017-03-11 15:11:46 +0900

    ESLintに沿ってコードを整える。

--- src/authenticate.js
+++ src/authenticate.js
@@ -1,82 +1,17 @@
 const crypto = require('crypto');
 
-function getRequestTokenSign(key, secret, callback) {
-  const requestUrl = 'https://api.twitter.com/oauth/request_token';
-  const callbackUrl = callback;
-  const consumer_key = key;
-  const consumer_secret = secret;
-  const keyOfSign = encodeURIComponent(consumer_secret) + '&';
-
-  let params = {
-    oauth_callback: callbackUrl,
-    oauth_consumer_key: consumer_key,
-    oauth_signature_method: 'HMAC-SHA1',
-    oauth_timestamp: (()=>{
-      const date = new Date();
-      return Math.floor( date.getTime() / 1000 ) ;
-    })(),
-    oauth_nonce: (()=>{
-      const date = new Date();
-      return date.getTime();
-    })(),
-    oauth_version: '1.0'
-  };
-
-  const dataOfSign = buildDataOfSign(params, requestUrl);
-	params['oauth_signature'] = buildSignature(keyOfSign, dataOfSign);
-	const headers = {
-		'Authorization': 'OAuth ' + buildHeaderParams(params)
-	};
-
-	return [requestUrl, headers];
-};
-
-function getAccessTokenSign(key, secret, requestTokenSecret, oauthToken, oauthVerifier){
-	const requestUrl = 'https://api.twitter.com/oauth/access_token';
-	const request_token_secret = requestTokenSecret;
-	const consumer_key = key;
-	const consumer_secret = secret;
-	const keyOfSign = encodeURIComponent(consumer_secret) + "&" + encodeURIComponent(request_token_secret);
-
-	let params = {
-		oauth_consumer_key: consumer_key,
-		oauth_token: oauthToken,
-		oauth_signature_method: 'HMAC-SHA1',
-		oauth_timestamp: (()=>{
-			const date = new Date();
-			return Math.floor( date.getTime() / 1000 ) ;
-		})(),
-		oauth_verifier: oauthVerifier,
-		oauth_nonce: (()=>{
-			const date = new Date();
-			return date.getTime();
-		})(),
-		oauth_version: '1.0'
-	};
-
-  const dataOfSign = buildDataOfSign(params, requestUrl);
-  params['oauth_signature'] = buildSignature(keyOfSign, dataOfSign);
-	const headers = {
-		'Authorization': 'OAuth ' + buildHeaderParams(params)
-	};
-
-	return [requestUrl, headers];
-}
-
 function buildDataOfSign(params, requestUrl) {
-  Object.keys(params).forEach(item => {
+  Object.keys(params).forEach((item) => {
     params[item] = encodeURIComponent(params[item]);
   });
-  let requestParams = Object.keys(params).map(item => {
-    return item + '=' + params[item];
-  });
-  requestParams.sort((a,b) => {
-    if( a < b ) return -1;
-    if( a > b ) return 1;
+  let requestParams = Object.keys(params).map(item => `${item}=${params[item]}`);
+  requestParams.sort((a, b) => {
+    if (a < b) return -1;
+    if (a > b) return 1;
     return 0;
   });
   requestParams = encodeURIComponent(requestParams.join('&'));
-  return encodeURIComponent('POST') + '&' + encodeURIComponent(requestUrl) + '&' + requestParams;
+  return `${encodeURIComponent('POST')}&${encodeURIComponent(requestUrl)}&${requestParams}`;
 }
 
 function buildSignature(keyOfSign, dataOfSign) {
@@ -85,10 +20,65 @@ function buildSignature(keyOfSign, dataOfSign) {
 }
 
 function buildHeaderParams(params) {
-  const headerParams = Object.keys(params).map(item => {
-    return item + '=' + params[item];
-  });
+  const headerParams = Object.keys(params).map(item => `${item}=${params[item]}`);
   return headerParams.join(',');
+}
+
+function getRequestTokenSign(key, secret, callbackUrl) {
+  const requestUrl = 'https://api.twitter.com/oauth/request_token';
+  const keyOfSign = `${encodeURIComponent(secret)}&`;
+
+  const params = {
+    oauth_callback: callbackUrl,
+    oauth_consumer_key: key,
+    oauth_signature_method: 'HMAC-SHA1',
+    oauth_timestamp: (() => {
+      const date = new Date();
+      return Math.floor(date.getTime() / 1000);
+    })(),
+    oauth_nonce: (() => {
+      const date = new Date();
+      return date.getTime();
+    })(),
+    oauth_version: '1.0',
+  };
+
+  const dataOfSign = buildDataOfSign(params, requestUrl);
+  params.oauth_signature = buildSignature(keyOfSign, dataOfSign);
+  const headers = {
+    Authorization: `OAuth ${buildHeaderParams(params)}`,
+  };
+
+  return [requestUrl, headers];
+}
+
+function getAccessTokenSign(key, secret, requestTokenSecret, oauthToken, oauthVerifier) {
+  const requestUrl = 'https://api.twitter.com/oauth/access_token';
+  const keyOfSign = `${encodeURIComponent(secret)}&${encodeURIComponent(requestTokenSecret)}`;
+
+  const params = {
+    oauth_consumer_key: key,
+    oauth_token: oauthToken,
+    oauth_signature_method: 'HMAC-SHA1',
+    oauth_timestamp: (() => {
+      const date = new Date();
+      return Math.floor(date.getTime() / 1000);
+    })(),
+    oauth_verifier: oauthVerifier,
+    oauth_nonce: (() => {
+      const date = new Date();
+      return date.getTime();
+    })(),
+    oauth_version: '1.0',
+  };
+
+  const dataOfSign = buildDataOfSign(params, requestUrl);
+  params.oauth_signature = buildSignature(keyOfSign, dataOfSign);
+  const headers = {
+    Authorization: `OAuth ${buildHeaderParams(params)}`,
+  };
+
+  return [requestUrl, headers];
 }
 
 module.exports.getRequestTokenSign = getRequestTokenSign;
